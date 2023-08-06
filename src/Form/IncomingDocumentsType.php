79e0fe512f4308ad19e171b1e66bc50404744226
qu <quqichbakich@mail.ru>
2023-08-06 14:12:10 +0300

    лапатим форму

--- src/Form/IncomingDocumentsType.php
+++ src/Form/IncomingDocumentsType.php
@@ -16,33 +16,36 @@ class IncomingDocumentsType extends AbstractType
     {
         $builder
             ->add('id_invoice', IntegerType::class, [
-                'label' => false,
+                'label' => 'Номер накладной',
                 'attr' => ['style' => 'width: 96px']
             ])
-            ->add('data_invoice', DateType::class)
+            ->add('data_invoice', DateType::class, [
+                'label' => 'Дата нак-ой',
+            ])
+
             ->add('name_detail', TextType::class, [
-                'label' => false,
+                'label' => 'Описание детали',
                 'attr' => ['style' => 'width: 96px']
             ])
             ->add('quantity', IntegerType::class, [
-                'label' => false,
+                'label' => 'Кол-во',
                 'attr' => ['style' => 'width: 96px']
             ])
             ->add('price', IntegerType::class, [
-                'label' => false,
+                'label' => 'Цена общая',
                 'attr' => ['style' => 'width: 96px']
             ])
             ->add('unit_price', IntegerType::class, [
-                'label' => false,
+                'label' => 'Цена шт',
                 'attr' => ['style' => 'width: 96px']
             ])
             ->add('quantity_sold', IntegerType::class, [
                 'label' => false,
-                'attr' => ['style' => 'width: 96px']
+                'attr' => ['style' => 'width: 96px', 'placeholder' => 'шт']
             ])
             ->add('price_sold', IntegerType::class, [
                 'label' => false,
-                'attr' => ['style' => 'width: 97px']
+                'attr' => ['style' => 'width: 97px', 'placeholder' => 'Цена']
             ]);
         //->add('id_counterparty', TextType::class)
         //->add('id_details', TextType::class)
